35ffbbee563e3e014667c9c2acc6df7cd5de7ff0
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2019-05-14 00:39:19 +0500

    Update cycle_mqtt_paw.php

--- scripts/cycle_mqtt_paw.php
+++ scripts/cycle_mqtt_paw.php
@@ -3,8 +3,8 @@ chdir(dirname(__FILE__) . '/../');
 include_once("./config.php");
 include_once("./lib/loader.php");
 include_once("./lib/threads.php");
-//include_once("./lib/websockets/sonoffws.class.php");
-include_once("./lib/websockets/client/lib/class.websocket_client.php");
+include_once("./lib/websockets/sonoffws.class.php");
+//include_once("./lib/websockets/client/lib/class.websocket_client.php");
 
 set_time_limit(0);
 // connecting to database
@@ -12,61 +12,100 @@ $db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME);
 include_once("./load_settings.php");
 include_once(DIR_MODULES . "control_modules/control_modules.class.php");
 $ctl = new control_modules();
-include_once(DIR_MODULES . 'paw/paw.class.php');
-$dev_sonoff_module = new dev_sonoff();
-$dev_sonoff_module->getConfig();
-$tmp = SQLSelectOne("SELECT ID FROM dev_sonoff_devices LIMIT 1");
-if (!$tmp['ID'])
-   exit; // no devices added -- no need to run this cycle
+include_once(DIR_MODULES . 'mqtt_paw/mqtt_paw.class.php');
+
+$paw_module = new mqtt_paw();
+$paw_module->getConfig();
+
+
+
+//if ($this->config['MQTT_ENABLE']<>"1")    exit; // no devices added -- no need to run this cycle
 echo date("H:i:s") . " running " . basename(__FILE__) . PHP_EOL;
 $latest_check=0;
-$checkEvery=$dev_sonoff_module->config['POLL_PERIOD'];
+//$checkEvery=$paw_module->config['POLL_PERIOD'];
+$checkEvery=1;
 //websockets
-$wssurl=$dev_sonoff_module->getWssUrl();
-$sonoffws = new SonoffWS($wssurl, $config);
+//$wssurl=$dev_sonoff_module->getWssUrl();
+//$sonoffws = new SonoffWS($wssurl, $config);
+//$ws=new WebsocketClient("http:\\localhost:8081/majordomo");
+debmes('cycle_starting', 'mqtt_paw');
+//$ws=new WebsocketClient("http://192.168.1.39", 8081, 'majordomo');
+//$ws=new WebsocketClient("http://localhost", 8081, 'majordomo');
+//$url="http://127.0.0.1:8081/majordomo";
+$url="wss://127.0.0.1:8081/majordomo";
+//$url="http://192.168.1.39:8081/majordomo";
+//$url="wss://192.168.1.39:8081/majordomo";
+//$url="wss://eu-pconnect2.coolkit.cc";
+
+//$url="wss://127.0.0.1:8081/majordomo";
+
+$ws=new SonoffWS($url, $config);
+$ws->socketUrl=$url;
+$ws->connect();
+
+
+debmes($ws, 'mqtt_paw');
 
 while (1)
 {
+   setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
+   echo date('Y-m-d H:i:s').' Polling devices...';
+/*
 //====================================HTTP POLLING===================================
    setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
    //http polling devices
    if ((time()-$latest_check)>$checkEvery) {
     $latest_check=time();
     echo date('Y-m-d H:i:s').' Polling devices...';
-    $dev_sonoff_module->processCycle();
-   }
+//    $paw_module->processCycle();
+//	   }
 //====================================END HTTP POLLING===============================  
 
+*/
+
 //====================================WSS POLLING====================================
- /*  setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
-    echo date('Y-m-d H:i:s').' Polling devices...';
-    	if($sonoffws->isConnected()) {
+//  setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
+
+    	if($ws->isConnected()) {
 		//выполняем если подключено
-		$read   = array($sonoffws->getSocket());
+                debmes('socket conected', 'mqtt_paw');
+		$read   = array($ws->getSocket());
 		$write  = NULL;
 		$except = NULL;
 		if (false === ($num_changed_streams = stream_select($read, $write, $except, $checkEvery))) {
 			// Обработка ошибок
-		} elseif ($num_changed_streams > 0) {
+		} 
+
+
+elseif ($num_changed_streams > 0) {
 			// Как минимум на одном из потоков произошло что-то интересное
-			$recv=$sonoffws->receive();
-			if($dev_sonoff_module->config['DEBUG']) {
-				debmes('[wss] +++ '.$recv, 'cycle_dev_sonoff_debug');
-			}
-			$dev_sonoff_module->wssRecv($recv, $sonoffws);
+debmes('receive', 'mqtt_paw');
+			$recv=$ws->receive();
+//			if($dev_sonoff_module->config['DEBUG']) {
+				debmes('[wss] +++ '.$recv, 'cycle_mqtt_paw_debug');
+//			}
+//			$dev_sonoff_module->wssRecv($recv, $sonoffws);
 		}
+
 		
-	} else {
+	} 
+
+else {
+debmes('reconect', 'mqtt_paw');
 		//переподключаемся
-		$sonoffws = new SonoffWS($wssurl, $config);
-		$sonoffws->socketUrl=$wssurl;
-		$sonoffws->connect();
-		if($sonoffws->isConnected()) {
-			$dev_sonoff_module->wssInit($sonoffws);
-		}
+		$ws = new SonoffWS($url, $config);
+		$ws->socketUrl=$url;
+		$ws->connect();
+//		if($ws->isConnected()) {
+//			$paw_module->wssInit($ws);
+//		}
 	}
-*/
+
+
+
 //====================================END WSS POLLING================================
+
+//}
 
 
    if (file_exists('./reboot') || IsSet($_GET['onetime']))
